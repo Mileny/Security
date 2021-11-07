a547d23f7b32cbeb996dc1922231c9c5e3ddd854
Mileny <Mileny@sina.com>
2021-11-07 14:05:53 +0800

    开始
    环境搭建

---
+++ Mi.Daily/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mi.Daily.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase {
+        public ActionResult Index() {
+            return Ok("Daily");
+        }
+    }
+}
---
+++ Mi.UserCenter/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mi.UserCenter.Controllers {
+
+    [Route("[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase {
+
+        public ActionResult Index() {
+            return Ok("成功");
+        }
+    }
+}
