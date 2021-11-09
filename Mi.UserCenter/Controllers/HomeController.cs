3a16d66ea5ac8aec0f974cae340c6d3d45a506b7
Mileny <Mileny@sina.com>
2021-11-09 21:30:52 +0800

    222

--- Mi.UserCenter/Controllers/HomeController.cs
+++ Mi.UserCenter/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ namespace Mi.UserCenter.Controllers {
     public class HomeController : ControllerBase {
 
         public ActionResult Index() {
-            return Ok("成功1111111");
+            return Ok("成功111111122222");
         }
     }
 }
